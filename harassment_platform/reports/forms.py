c41505440385c98cad5a55b17cedee6d4ab5d8a8
Abhiraj Adhikary <abhirajadhikary06@gmail.com>
2025-01-22 23:57:01 +0530

    Commented out the Victim OR Non-Victim part

--- harassment_platform/reports/forms.py
+++ harassment_platform/reports/forms.py
@@ -4,7 +4,7 @@ from .models import HarassmentReport
 class HarassmentReportForm(forms.ModelForm):
     class Meta:
         model = HarassmentReport
-        fields = ['location', 'time', 'date', 'harassment_type', 'reported_by' 'description']
+        fields = ['location', 'time', 'date', 'harassment_type', 'description']
         widgets = {
             'time': forms.TimeInput(attrs={'placeholder': 'e.g., 10:30:00'}),
             'date': forms.DateInput(attrs={'placeholder': 'e.g., 2025-01-01'}),
--- harassment_platform/reports/models.py
+++ harassment_platform/reports/models.py
@@ -21,11 +21,13 @@ class HarassmentReport(models.Model):
     location = models.CharField(max_length=100)
     description = models.TextField()
     harassment_type = models.CharField(max_length=100, choices=HARASSMENT_TYPE_CHOICES)
+    '''
     REPORTER_TYPE_CHOICES = [
         ('victim', 'Victim'),
         ('eyewitness', 'Eyewitness'),
     ]
     reported_by = models.CharField(max_length=50, choices=REPORTER_TYPE_CHOICES, default='victim')
+    '''
     timestamp = models.DateTimeField(auto_now_add=True)
     latitude = models.FloatField(null=True, blank=True)  # New field for latitude
     longitude = models.FloatField(null=True, blank=True)  # New field for longitude
